c1dfbb28816c5ef327642e495e3da9444b94254a
briancliau <brian.liau@tufts.edu>
2025-04-28 14:10:35 -0400

    Add files via upload

--- Tuner_App/FFT/main.c
+++ Tuner_App/FFT/main.c
@@ -1,124 +1,77 @@
 #define ARMCM4
 #include <arm_math.h>
-#include <stm32l432xx.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "ee14lib.h"
+#include <stm32l432xx.h>
+#include <math.h>
 
-// void* aligned_malloc(size_t size, size_t alignment);
+#define SAMPLE_RATE 10000  // Sampling rate in Hz
+#define NUM_SAMPLES 2048   // Number of samples
+#define FREQ 440          // Sine wave frequency in Hz
 
-// int main(void) {
-//     while(1) {
+uint8_t fftFlag = 1;
+arm_rfft_instance_q15 fftHandler; // instance of rfft
+uint32_t dataLength = 2048; // length of data
+uint32_t ifftFlag; // set to 0 to do FFT
+uint32_t bitReverse; // set to 0 to not reverse order of bits
+uint32_t sampleRate = SAMPLE_RATE;
 
-//     }
-//     return 0;
-// }
+q15_t sample[NUM_SAMPLES];
+q15_t result[NUM_SAMPLES * 2];//[8192];
 
-#define AUDIO_BUFFER_SIZE 256
-#define FFT_BUFFER_SIZE 1024
-#define SAMPLE_RATE_HZ 1024.0f
 
-float *fft_buffer;
-arm_rfft_fast_instance_f32 fftHandler;
 
-float fftBufIn[FFT_BUFFER_SIZE];
-float fftBufOut[FFT_BUFFER_SIZE];
-uint16_t BUFFER_GETTING_AUDIO = AUDIO_BUFFER_SIZE;
-uint16_t INT16_TO_FLOAT = 0;
-float inBufPtr[AUDIO_BUFFER_SIZE];
-uint8_t fftFlag = 0;
-uint8_t processHalfFlag = 1;
-uint32_t fftLenReal = 8192;
-uint32_t ifftFlagR = 0;
-uint32_t bitReverseFlag = 0;
-
-// // // Function called when half of buffer is filled in
-// // void Process_HalfBuffer() {
-
-// //     static float leftIn, rightIn;
-// //     static float leftProcessed, rightProcessed;
-// //     static int16_t leftOut, rightOut;
-
-// //     //Loop through half of double buffer
-// //     for (uint8_t n = 0; n < (BUFFER_GETTING_AUDIO / 2) - 1; n += 2) {
-// //         // convert to float
-// //         leftIn = INT16_TO_FLOAT * ((float) inBufPtr[n]);
-// //         rightIn = INT16_To_Float * ((float) inBufPtr[n+1]);
-
-// //         // apply processing to input
-// //         leftProcessed = leftIn;
-// //         rightProcessed = rightIn;
-
-// //         // convert processed float to 16 bit integers for output buffer
-// //         leftOut = (int16_t) (FLOAT_TO_INT16 * outputVolume * leftProcessed);
-// //         rightOut = (int16_t) (FLOAT_TO_INT16 * outputVolume * rightProcessed);
-
-// //         // Set output buffer size 
-// //         outBufPtr[n] = leftOut;
-// //         outBufPtr[n + 1] = rightOut;
-// //     }
-// // }
-
-// void FFT_ready() {
-
-//     static float leftIn, rightIn;
-//     static float leftProcessed, rightProcessed;
-//     static int16_t leftOut, rightOut;
-//     static int16_t fftIndex = 0;
-
-//     //Loop through half of double buffer
-//     for (uint8_t n = 0; n < (BUFFER_GETTING_AUDIO / 2) - 1; n += 2) {
-//         // convert to float
-//         leftIn = INT16_TO_FLOAT * ((float) inBufPtr[n]);
-//         rightIn = INT16_TO_FLOAT * ((float) inBufPtr[n+1]);
-
-//         // Fill FFt buffer
-//         fftBufIn[fftIndex] = leftIn;
-//         fftIndex++;
-
-//         if (fftIndex == FFT_BUFFER_SIZE) {
-//             // perform FFT
-//             //arm_rfft_fast_f32(&fftHandler, &fftBufIn, &fftBufOut, 0);
-
-//             // set FFT flag
-//             fftFlag = 1;
-
-//             // Reset FFT array index
-//             fftIndex = 0;
-//         }
-//     }   
-// }
-
-int main(void) { 
-    // initialize FFT
-    //arm_rfft_init_q15(&fftHandler, fftLenReal, ifftFlagR, bitReverseFlag);
-    arm_rfft_fast_init_32_f32(&fftHandler);
+int main() {
+    host_serial_init();
+    char buffer[100];
+    sprintf(buffer, "Hello World\n");
+    serial_write(USART2, buffer, strlen(buffer));
+    dataLength = NUM_SAMPLES; // set to 8192 arbitrarily 
+    ifftFlag = 0;
+    bitReverse = 1;
+    arm_rfft_init_q15(&fftHandler, dataLength, ifftFlag, bitReverse);
     float peakVal = 0.0f;
-    uint16_t peakHz = 0;
+    uint32_t peakHz = 0;
+
+    // signal generator
+    float sine_wave[NUM_SAMPLES];
+    
 
     while (1) {
-
-        if (processHalfFlag) {
-            //Process_HalfBuffer();
-            processHalfFlag = 0;
+        for (int i = 0; i < NUM_SAMPLES; i++) {
+            sine_wave[i] = sinf(2.0f * PI * FREQ * i / SAMPLE_RATE);
+        }
+    
+    
+        for (int i = 0; i < NUM_SAMPLES; i++) {
+            sample[i] = (q15_t)(sine_wave[i] * 32767);
         }
 
         if (fftFlag) {
-            // compute absolute value of complex FFT results to get peak
             peakVal = 0.0f;
-            peakHz = 0.0f;
+            peakHz = 0;
+
+            arm_rfft_q15(&fftHandler, sample, result);
 
             uint32_t freqIndex = 0;
 
-            for (uint32_t index = 0; index < FFT_BUFFER_SIZE; index += 2) {
-                float curVal = sqrtf(fftBufOut[index] * fftBufOut[index] + (fftBufOut[index + 1]) * fftBufOut[index + 1]);
+            for (uint32_t index = 0; index < dataLength; index += 2) {
+                float real = result[index] / 32768.0f;
+                float imag = result[index + 1] / 32768.0f;
+                float curVal = sqrtf(real * real + imag * imag);
 
                 if (curVal > peakVal) {
                     peakVal = curVal;
-                    peakHz = (uint16_t) (freqIndex * SAMPLE_RATE_HZ / ((float) FFT_BUFFER_SIZE));
+                    freqIndex = index / 2;
+                    peakHz = (uint32_t) (freqIndex * sampleRate / ((float) dataLength));
                 }
 
                 freqIndex++;
             }
+            sprintf(buffer, "peakHz: %ld\n", peakHz);
+            serial_write(USART2, buffer, strlen(buffer));
         }
-
     }
 }
